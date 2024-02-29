6b2eb39deab4b01ed766373c1fff41de2d5e570a
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-02-29 18:13:06 +0530

    updated code

---
+++ js/validation.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const conformpassword = document.getElementById("Cpassword");
+const email = document.getElementById("email");
